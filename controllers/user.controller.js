cef1bf12c598bc10ff2f57e5d8c2b0272a54839f
Manish Sahani <manishsahani345@gmail.com>
2024-12-03 16:27:37 +0530

    Authentication, Model and controller setup

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,9 +1,80 @@
-const mongoose = require("mongoose");
+const { User } = require('../models/user.model');
 
-const signupUser = async (req, res) => {
+const signUpUser = async (req, res) => {
+    try {
 
-    const userData = req.body;
+    } catch (error) {
 
-    const existingUser = await User.find();
+    }
 
+}
+
+const loginUser = async (req, res) => {
+
+    try {
+
+    } catch (error) {
+
+    }
+}
+
+const getCurrentUser = async (req, res) => {
+    try {
+
+    } catch (error) {
+
+    }
+}
+
+const getProfile = async (req, res) => {
+    try {
+
+    } catch (error) {
+
+    }
+
+}
+
+const signOutUser = async (req, res) => {
+    try {
+
+    } catch (error) {
+
+    }
+
+}
+
+const verifyEmail = async (req, res) => {
+    try {
+
+    } catch (error) {
+
+    }
+}
+
+const updateUser = async (req, res) => {
+    try {
+
+    } catch (error) {
+
+    }
+}
+
+const deleteUser = async (req, res) => {
+    try {
+
+    } catch (error) {
+
+    }
+}
+
+module.exports = {
+    signUpUser,
+    loginUser,
+    getCurrentUser,
+    getProfile,
+    signOutUser,
+    verifyEmail,
+    updateUser,
+    deleteUser
 }
--- middlewares/auth.middleware.js
+++ middlewares/auth.middleware.js
@@ -1,11 +1,29 @@
 const jwt = require('jsonwebtoken');
 const { CustomError } = require('./errorHandler');
+const { User } = require('../models/user.model');
 
 const verifyJWT = async (req, res, next) => {
-    const token = req.cookies?.accessToken || req.header("Authentication")?.replace("Bearer", "");
+    try {
+        const token = req.cookies?.accessToken || req.header("Authentication")?.replace("Bearer", "");
 
-    if (!token) {
-        throw new CustomError(401, "Unauthorized request");
+        if (!token) {
+            throw new CustomError(401, "Unauthorized request");
+        }
+
+        const decodedData = await jwt.verify(token, process.env.SECRET_KEY);
+
+        const user = await User.findById(decodedData._id).select("-password");
+
+        if (!user) {
+            throw new CustomError(404, "Invalid Access Token!");
+        }
+
+        req.user = user;
+        next();
+    } catch (error) {
+        console.log("Error verifying user", err.message);
+        throw new CustomError(error.statusCode || 500, error.message);
     }
 };
 
+module.exports = { verifyJWT };
