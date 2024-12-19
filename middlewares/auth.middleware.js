a3dd73150645ad2144105bac2406f3f07e766acb
Manish Sahani <manishsahani345@gmail.com>
2024-12-19 12:06:10 +0530

    middleware modification

--- middlewares/auth.middleware.js
+++ middlewares/auth.middleware.js
@@ -4,14 +4,17 @@ const { User } = require('../models/user.model');
 const { STATUS_CODES } = require('../utils/constants');
 
 const verifyJWT = async (req, _, next) => {
-    // const token = req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer ", "");
-    const token = req.header("Authorization").split(" ")[1];
+    const authHeader = req.header("Authorization");
+    if (!authHeader) {
+        return next(new CustomError(STATUS_CODES.BAD_REQUEST, "BAD_REQUEST : No token provided"));
+    }
 
+    const token = authHeader.split(" ")[1];
     if (!token) {
         return next(new CustomError(STATUS_CODES.BAD_REQUEST, "BAD_REQUEST : No token provided"));
     }
-    try {
 
+    try {
         const decodedData = await jwt.verify(token, process.env.SECRET_KEY);
 
         const user = await User.findById(decodedData?._id).select("_id username email");
@@ -29,10 +32,10 @@ const verifyJWT = async (req, _, next) => {
             return next(new CustomError(STATUS_CODES.UNAUTHORIZED, "Unauthorized : Token has expired"));
         } else if (error.name === "JsonWebTokenError") {
             return next(new CustomError(STATUS_CODES.BAD_REQUEST, "BAD_REQUEST : Invalid token"));
+        } else {
+            return next(new CustomError(STATUS_CODES.INTERNAL_SERVER_ERROR, "INTERNAL_SERVER_ERROR : An error occurred"));
         }
-
-        next(error);
     }
 };
 
-module.exports = { verifyJWT };
+module.exports = verifyJWT;
